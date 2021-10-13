3b64f4610b356ee1182240d5b1cc19ed6b618585
Bavido <viviendonald8@gmail.com>
2021-10-13 18:53:21 -0200

    creation of the page book,designing of the home page and the book page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'book',
+    loadChildren: () => import('./book/book.module').then( m => m.BookPageModule)
   },
+
 ];
 
 @NgModule({
